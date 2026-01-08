fc1c45327ce711c20af35b6c8d451cbb51e666e0
Melih Kökçü <transferise.com@gmail.com>
2026-01-08 16:27:02 +0300

    fix github pages path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
+    pathPrefix: "/transferiseguide/",
     dir: {
       input: "src",
       output: "_site"
